b60a707798007a6256121069569a52fdc2c4d726
Ahmad Raza Khokhar <ahmadrazawebexpert@gmail.com>
2023-12-10 16:07:20 +0000

    .

--- Contact/page.js
+++ Contact/page.js
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-
-export default function Contact() {
-  return (
-    <div>
-      <h1>Hello Page</h1>
-    </div>
-  );
-=======
 export const Contact = ()=> {
     return <>
     <div>
         Hi
     </div>
     </>
->>>>>>> d26b1d9 (Update)
 }
