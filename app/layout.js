f2b1a8e0cfa7e2faa8098be9de7f85579739129f
Saeed Ahmad Bloach <136958120+saeedahmad572@users.noreply.github.com>
2023-12-10 18:52:39 +0000

    update

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       {/* <head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"/>
+        <link  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"/>
         <script src="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" type='module'></script>
       </head> */}
       <body className={inter.className}>
