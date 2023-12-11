8b9e165c0f9c5c932c6df719ec8b8f58c8c2c1de
Ahmad Raza Khokhar <ahmadrazawebexpert@gmail.com>
2023-12-11 04:59:00 +0000

    Update in COnfog file

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['*'],
-      },
+  images: {
+    remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: '**',
+            port: '',
+            pathname: '**',
+        },
+    ],
+},
 }
 
 module.exports = nextConfig
